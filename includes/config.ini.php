85f04b6e5e107a73e5b82b0e179d3964b3594fb1
sergio <svera@microcitios.net>
2017-07-25 11:57:49 -0500

    conecion a la base de datos

--- includes/config.ini.php
+++ includes/config.ini.php
@@ -1,5 +1,10 @@
 <?php
 
+ $conexion=new mysqli("localhost","root","","prueba3");
+   if ($conexion->connect_error) {
+      echo "fallo al conectarse (".$mysqli->connect_error.")".$mysqli->connect_error;
+   }
+
 include("libs/smarty.class.php");
 $smarty=new smarty(); 
 
