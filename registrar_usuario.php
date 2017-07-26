f5704fbf53cbc5e7a7f3a7794faa8f8b29da40b8
sergio <svera@micrositios.net>
2017-07-26 13:47:18 -0500

    se crearon los altertas

--- registrar_usuario.php
+++ registrar_usuario.php
@@ -6,9 +6,26 @@ $Identificacion=$_POST['Identificacion'];
 $Direccion=$_POST['Direccion'];
 $contrasena=$_POST['contrasena'];
 $con_contrasena=$_POST['con_contrasena'];
+
+
  if ($contrasena!=$con_contrasena) {
    $alerta= "<div class='alert alter-danger'role='alert'>las contraseñas no coinciden</div>"
    $smarty->assign("alerta,$alerta");
+ }else{
+     include("config.ini.php");
+
+     $id dechex($id);
+     $contrasena=md5($contrasena);
+     $con_contrasena=md5($con_contrasena);
+
+    $registro=mysqli_query($conexion,"INSERT INTO usuario(id,nombre,apellido,Identificacion,Direccion,contrasena,con_contrasena)
+    	VALUES('$id','$nombre','$apellido','$Identificacion','$Direccion','$contrasena','$con_contrasena')")or die(mysqli_error($consulta));
+
+    if ($registro) {
+    	$alerta= "<div class='alert alter-susses'role='alert'>se registro correctamente</div>"
+    }else{
+    	$alerta= "<div class='alert alter-danger'role='alert'>no se regitro</div>"
+    }
  }
 
 $smarty->display("header.tpl");
